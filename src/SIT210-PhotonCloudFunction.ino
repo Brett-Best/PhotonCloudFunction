88ee84c2999e06b2015d3299660b7f1339e30c25
Brett Best <brettbest@live.com.au>
2019-03-31 01:52:15 +1100

    Added initial files.

---
+++ src/SIT210-PhotonCloudFunction.ino
@@ -0,0 +1,17 @@
+/*
+ * Project SIT210-PhotonCloudFunction
+ * Author: Brett T Best
+ * Date: March 2019
+ */
+
+#include <Particle.h>
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+
+}
