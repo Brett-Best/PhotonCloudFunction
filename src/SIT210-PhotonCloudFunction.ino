084f53b61bbaa66a78be319dd9c493515e074b31
Brett Best <brettbest@live.com.au>
2019-03-31 02:11:32 +1100

    Added code to create cloud function and manage leds.

--- src/SIT210-PhotonCloudFunction.ino
+++ src/SIT210-PhotonCloudFunction.ino
@@ -6,12 +6,42 @@
 
 #include <Particle.h>
 
+int toggleLED(String led);
+
 void setup()
 {
-  Serial.begin(9600);
+  Particle.function("toggleLED", toggleLED);
+
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
 }
 
 void loop()
 {
 
+}
+
+void togglePin(int pin)
+{
+  digitalWrite(pin, !digitalRead(pin));
+}
+
+// this function automagically gets called upon a matching POST request
+int toggleLED(String led)
+{
+  if (led == "blue")
+  {
+    togglePin(D0);
+  }
+  else if (led == "green")
+  {
+    togglePin(D1);
+  }
+  else if (led == "red")
+  {
+    togglePin(D2);
+  }
+
+  return 1;
 }
